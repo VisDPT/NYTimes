224f889e1104ae72c06602566d02c8ed93a0d5b3
Dr. Visalam Mahalingam PT <Jyo@Visalams-MacBook-Air.local>
2016-08-27 14:09:29 -0500

    V4

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -28,10 +28,10 @@ $(document).on("ready", function() {
 
 $("#submit").on('click', function(ev){
 	ev.preventDefault();
-        var query="university";
-        var records;
-        var startYear;
-        var endYear;
+        var query=$("#search").val();
+        var records=$("#records").val();
+        var startYear=$("#start-year").val();
+        var endYear=$("#end-year").val();
         var apiKey = "66b2b97b3a12440c9505e3662f4f2b47";
 
         var queryURL ="https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+ apiKey;
@@ -41,6 +41,18 @@ $("#submit").on('click', function(ev){
             console.log("query is empty");
         } else{
             queryURL = queryURL + "&q="+query;
+        }
+ 
+        if(records.trim() === ""){
+            console.log("records is empty");
+        } else{
+            queryURL = queryURL + "&page="+records;
+        }
+        if(startYear.trim() !== ""){
+            queryURL = queryURL + "&begin_date="+startYear;
+        }
+        if(endYear.trim() !== ""){
+            queryURL = queryURL + "&end_date="+endYear;
         }
 /*
         if(records.trim() === ""){
