b92675ba2f7cf8c8a249ca5b543fbab7d939b861
Dr. Visalam Mahalingam PT <Jyo@Visalams-MacBook-Air.local>
2016-08-27 13:51:38 -0500

    V2

---
+++ assets/js/logic.js
@@ -0,0 +1,66 @@
+/*Instructions - Phase 1:
+
+Front-End Team:
+
+Create the layout for the design above. Feel free to use Bootstrap or to do something more customized of your own.
+
+Make sure to organize your code so as to have the necessary IDs.
+
+Incorporate text boxes for capturing User Input. Then research how to retrieve the input values in Javascript
+
+Back-End Team:
+
+Do preliminary research on the API.
+NYT-API
+
+Register for an API Key.
+
+Understand what format the URL should look like to make an Article Call. (Hint: Use the API Console!!)
+
+Experiment with console logging various fields. */
+
+
+///
+console.log("TEST");
+//$(document).on("ready", function() {
+    
+
+
+$("#submit").on('click', function(){
+        var query="university";
+        var records;
+        var startYear;
+        var endYear;
+        var apiKey = "66b2b97b3a12440c9505e3662f4f2b47";
+
+        var queryURL ="https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+ apiKey;
+
+        console.log('working sort of')
+        if(query.trim() === ""){
+            console.log("query is empty");
+        } else{
+            queryURL = queryURL + "&q="+query;
+        }
+/*
+        if(records.trim() === ""){
+            console.log("query is empty");
+        } else{
+            queryURL = queryURL + "&q="+records;
+        }
+*/
+        $.ajax({
+            url:queryURL,
+            method: 'GET'
+        }).done(function(res){
+            console.log(res);
+            console.log(queryURL);
+        });
+
+        return false;
+    });
+
+
+
+
+
+//}); 
